5981d474bb8b5a9e206c9265581fc1a711a4dc7d
KaMeHb-UA <marlock@bassteam.ml>
2020-11-11 00:27:02 +0200

    initial

---
+++ index.js
@@ -0,0 +1,20 @@
+import Stream from '@mfelements/stream'
+
+export default async function getCameraVideo(options){
+    const streamId = await mainThreadAction('createStream');
+    let stream = new Stream;
+    streamStorage[streamId] = stream;
+    await mainThreadAction('getUserMedia', streamId, options);
+    return new ReadableStream({
+        start(controller){
+            stream.on('data', chunk => controller.enqueue(chunk));
+            stream.on('error', e => controller.error(e));
+            stream.on('end', () => controller.close())
+        },
+        async cancel(){
+            await endStream(streamId);
+            delete streamStorage[streamId];
+            stream = null
+        },
+    })
+}

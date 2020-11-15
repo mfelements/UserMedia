359efd195173ff293ff02457b8ab9ab55fe197aa
KaMeHb-UA <marlock@bassteam.ml>
2020-11-15 15:26:06 +0200

    fixed typings

--- canvas-wrapped.d.ts
+++ canvas-wrapped.d.ts
@@ -1,8 +1,8 @@
 import { CanvasKit } from 'canvaskit-wasm'
 import { Metadata } from './@types/shared'
 
-export default async function canvasWrapped<T extends Metadata>(params: { metadata: T, stream: ReadableStream }): {
+export default function canvasWrapped<T extends Metadata>(params: { metadata: T, stream: ReadableStream }): Promise<{
     metadata: T;
     canvas: ReturnType<CanvasKit['MakeCanvas']>;
     stopRender(): void;
-}
+}>

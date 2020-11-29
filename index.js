32c0b3ab33824ab6e09493f9b60251cc7cfb5a76
KaMeHb-UA <marlock@bassteam.ml>
2020-11-29 21:17:53 +0200

    reimplement ReadableMediaStream
    mfelements/mfelements.github.io#8

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-import Stream from '@mfelements/stream'
+import { ReadableStream } from '@mfelements/stream-definitions'
 
-export async function getCameraVideo(options){
-    const streamId = await mainThreadAction('createStream');
-    let stream = new Stream;
-    streamStorage[streamId] = stream;
-    const metadata = await mainThreadAction('getCameraVideo', streamId, options);
-    return {
-        metadata,
-        stream: new ReadableStream({
-            start(controller){
-                stream.on('data', chunk => controller.enqueue(chunk));
-                stream.on('error', e => controller.error(e));
-                stream.on('end', () => controller.close())
-            },
-            async cancel(){
-                await endStream(streamId);
-                delete streamStorage[streamId];
-                stream = null
-            },
-        }),
+const start = Symbol();
+
+class ReadableMediaStream extends ReadableStream{
+    /** @type {Promise<string>} */
+    #streamId;
+    /** @type {Promise<void>} */
+    #started;
+    /** @type {boolean} */
+    #stopped;
+    /** @arg {Promise<string>} streamId */
+    constructor(streamId){
+        this.#streamId = streamId;
+        this.#started = new Promise(r => this[start] = r)
     }
+    async *[Symbol.asyncIterator](){
+        /** @type {{ done: boolean, value: any }} */
+        let currentValue;
+        await this.#started;
+        while(currentValue = await mainThreadAction('getStreamChunk', await this.#streamId), !currentValue.done && !this.#stopped)
+            // hello Python :)
+            yield currentValue.value
+    }
+    async stop(){
+        this.#stopped = true;
+        await mainThreadAction('stopStream', await this.#streamId)
+    }
+}
+
+export function getCameraVideo(options){
+    const streamId = mainThreadAction('createDirtyStream');
+    const stream = new ReadableMediaStream(streamId);
+    streamId
+        .then(streamId => mainThreadAction('getCameraVideo', streamId, options))
+        .then(stream[start]);
+    return stream
 }
